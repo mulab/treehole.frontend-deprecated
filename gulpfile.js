08cac8ee3bf59a938b4b77971553cbdfc3037ffd
Zhipeng Jia <zhipeng.jia@outlook.com>
2015-03-04 20:06:58 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,19 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var plumber = require('gulp-plumber');
+var rename = require("gulp-rename");
 var path = require('path');
 var webpack = require('webpack');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
+var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+
+function errorHandler(moduleName) {
+  return function (err) {
+    gutil.log('[' + moduleName + ']', err.message);
+    this.emit('end');
+  };
+}
 
 function webpackCallback(callback) {
   return function (err, stats) {
@@ -21,16 +30,12 @@ function webpackCallback(callback) {
 }
 
 gulp.task('build-stylesheets', function () {
-  return gulp.src('./assets/stylesheets/mobile.less')
-    .pipe(plumber({
-      errorHandler: function (err) {
-        gutil.log('[Less]', err.message);
-        this.emit('end');
-      }
-    }))
+  return gulp.src('./assets/stylesheets/mobile/index.less')
+    .pipe(rename('mobile.bundle.less'))
+    .pipe(plumber({ errorHandler: errorHandler('Less') }))
     .pipe(less({
       paths: [
-        path.join(__dirname, 'assets', 'stylesheets'),
+        path.join(__dirname, 'assets', 'stylesheets', 'mobile'),
         path.join(__dirname, 'bower_components', 'onsenui', 'build', 'css')
       ]
     }))
@@ -45,7 +50,14 @@ gulp.task('build-scripts-dev', function (callback) {
 });
 
 gulp.task('build-scripts', function (callback) {
-  // TODO
+  var config = require('./webpack.config');
+  config.plugins.push(new ngAnnotatePlugin({ add: true }));
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }));
+  webpack(config, webpackCallback(callback));
 });
 
 gulp.task('build-assets-dev', ['build-stylesheets', 'build-scripts-dev']);
