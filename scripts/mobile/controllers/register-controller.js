5c477a15619d3474ac5a5aab7cc4d75fdcb85ba7
Zhipeng Jia <zhipeng.jia@outlook.com>
2015-04-21 22:05:03 +0800

    Register page

--- scripts/mobile/controllers/register-controller.js
+++ scripts/mobile/controllers/register-controller.js
@@ -4,24 +4,35 @@ var Util = require('../util');
 var _ = require('lodash');
 
 module.exports = function ($scope) {
-  $scope.waitingLogin = false;
-
-  function resetButton() {
-    $scope.apply(function () {
-      $scope.waitingLogin = false;
-    });
-  }
-
-  $scope.login = function () {
+  $scope.register = function () {
     var username = $scope.username;
     var password = $scope.password;
+    var nickname = $scope.nickname;
+
     $scope.waitingLogin = true;
     var errorMessages = [];
     if (_.isEmpty(username)) {
       errorMessages.push('用户名不能为空！');
+    } else {
+      if (!(3 <= username.length && username.length <= 10)) {
+        errorMessages.push('用户名长度必须在3到10之间！');
+      }
+      if (!username.match(/^[a-zA-Z0-9_\.]*$/)) {
+        errorMessages.push('用户名包含非法字符！');
+      }
+    }
+    if (_.isEmpty(nickname)) {
+      errorMessages.push('昵称不能为空！');
+    } else {
+      if (!(3 <= nickname.length && nickname.length <= 15)) {
+        errorMessages.push('昵称长度必须在3到15之间！');
+      }
     }
     if (_.isEmpty(password)) {
       errorMessages.push('密码不能为空！');
+    }
+    if (password !== $scope.passwordConfirm) {
+      errorMessages.push('两次输入的密码不一样！');
     }
 
     if (!_.isEmpty(errorMessages)) {
@@ -32,14 +43,13 @@ module.exports = function ($scope) {
       });
     }
 
-    AV.User.logIn(username, password).
+    AV.User.signUp(username, password, { nickname: nickname }).
       then(function () {
         navi.resetToPage('hole/list.html', { animation: 'fade' });
       }, function (err) {
         Util.showErrorAlert(Util.translate(err.message), function () {
           $scope.$apply(function () {
             $scope.waitingLogin = false;
-            $scope.password = '';
           });
         });
       });
---
+++ scripts/mobile/util.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var _ = require('lodash');
+
+exports.showErrorAlert = function (message, callback) {
+  var options = {
+    message: message,
+    animation: 'none',
+    title: '错误',
+    buttonLabel: '确定'
+  };
+  if (_.isFunction(callback)) {
+    options.callback = callback;
+  }
+  ons.notification.alert(options);
+};
+
+var TABLE = {
+  'Username has already been taken': '用户名已被占用！',
+  'Could not find user': '用户名或密码错误！',
+  'The username and password mismatch.': '用户名或密码错误！'
+};
+
+exports.translate = function (message) {
+  if (message in TABLE) {
+    message = TABLE[message];
+  }
+  return message;
+};
