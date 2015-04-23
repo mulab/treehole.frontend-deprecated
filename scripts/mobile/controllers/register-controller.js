949316619024a5c9ecfe165ff754723d1e31a1c9
Zhipeng Jia <zhipeng.jia@outlook.com>
2015-04-24 02:15:06 +0800

    Tiny change in register page

--- scripts/mobile/controllers/register-controller.js
+++ scripts/mobile/controllers/register-controller.js
@@ -6,7 +6,7 @@ var _ = require('lodash');
 module.exports = function ($scope) {
   function onProcessingEnd() {
     $scope.$apply(function () {
-      $scope.waitingLogin = false;
+      $scope.waitingSubmit = false;
       $scope.password = '';
       $scope.passwordConfirm = '';
     });
@@ -17,7 +17,7 @@ module.exports = function ($scope) {
     var password = $scope.password;
     var nickname = $scope.nickname;
 
-    $scope.waitingLogin = true;
+    $scope.waitingSubmit = true;
     var errorMessages = [];
     if (_.isEmpty(username)) {
       errorMessages.push('用户名不能为空！');
@@ -26,14 +26,14 @@ module.exports = function ($scope) {
         errorMessages.push('用户名长度必须在3到10之间！');
       }
       if (!username.match(/^[a-zA-Z0-9_\.]*$/)) {
-        errorMessages.push('用户名包含非法字符！');
+        errorMessages.push('用户名只能包含英文字母、数字、圆点(.)和下划线！');
       }
     }
     if (_.isEmpty(nickname)) {
       errorMessages.push('昵称不能为空！');
     } else {
-      if (!(3 <= nickname.length && nickname.length <= 15)) {
-        errorMessages.push('昵称长度必须在3到15之间！');
+      if (!(1 <= nickname.length && nickname.length <= 15)) {
+        errorMessages.push('昵称长度必须在1到15之间！');
       }
     }
     if (_.isEmpty(password)) {
