ef856e46b9183db345f1867b9f40a432cc02537d
Zhipeng Jia <zhipeng.jia@outlook.com>
2015-04-25 16:50:31 +0800

    Pull to refresh in hole's list

--- scripts/mobile/controllers/hole/hole-list-controller.js
+++ scripts/mobile/controllers/hole/hole-list-controller.js
@@ -1,17 +1,30 @@
 'use strict';
 
 var Hole = require('models/hole');
+var _ = require('lodash');
 
-module.exports = function ($scope) {
+module.exports = function ($scope, $timeout) {
+  function refresh(callback) {
+    var query = new AV.Query(Hole);
+    query.descending('createdAt');
+    query.include('author');
+    query.include('images');
+    query.find().then(function (holes) {
+      $scope.holes = holes;
+      if (_.isFunction(callback)) {
+        callback();
+      }
+    });
+  }
+
   $scope.user = AV.User.current();
-
   $scope.hole = [];
-  var query = new AV.Query(Hole);
-  query.descending('createdAt');
-  query.include('author');
-  query.include('images');
-  query.find().then(function (holes) {
-    $scope.holes = holes;
+  refresh(function () {
     $scope.$apply();
   });
+  $scope.refresh = function ($done) {
+    $timeout(function () {
+      refresh($done);
+    }, 1000);
+  };
 };
