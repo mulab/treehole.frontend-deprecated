4c1cee16377d405389564a4dea02749ad0d03b4c
Zhipeng Jia <zhipeng.jia@outlook.com>
2015-05-05 16:51:25 +0800

    Switch production to treehole-rev1

--- config/production.js
+++ config/production.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var CONFIG = {
-  appId: 'mc7lzq08gf5rmn1e96pleshhbpgcsr2m9u1e94hv1h1744py',
-  appKey: 'cwvd7d830sbjdc1k6ci3u8b5siko2k2obbgqfeccig9h3hr5',
+  appId: 'f5hpr27uupdxiaw41msqm7lzydgxjmy0721qrfjdpoqhnn5p',
+  appKey: 'jdn4ayl6fuztlmagd9z8oax5tr03qgvb4u1rxp8x66tqv20u',
   defaultAvatar: '/assets/default-avatar.png',
   isProduction: true
 };
